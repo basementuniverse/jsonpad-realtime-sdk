b0ec5cb5233f4b451780369bde7611d70a0f7993
basementuniverse <gordonlarrigan@googlemail.com>
2024-12-14 23:32:24 +0000

    Try to parse model data for item events

--- src/events/item-event.ts
+++ src/events/item-event.ts
@@ -2,6 +2,11 @@ import { EventDetail, Item, MessageType } from '../types';
 
 export class ItemEvent extends CustomEvent<any> {
   constructor(messageType: MessageType, detail: EventDetail<Item>) {
+    if (detail.model.data && typeof detail.model.data === 'string') {
+      try {
+        detail.model.data = JSON.parse(detail.model.data);
+      } catch (error) {}
+    }
     super(messageType, { detail });
   }
 }
